b808ed1822d6755e59edc6fb347f6e43025a05dd
geraldpnarisma <geraldpnarisma@gmail.com>
2025-04-17 16:39:14 +0800

    Added send message function

--- index.js
+++ index.js
@@ -53,33 +53,33 @@ document.getElementById('contactForm').addEventListener('submit', async function
   const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
 
   if (!fullname) {
-      formMessage.innerHTML = `<div class="alert alert-danger">Full Name is required.</div>`;
+      formMessage.innerHTML = `<div class="alert alert-danger text-center">Full Name is required.</div>`;
       return;
   }
 
   if (!nameRegex.test(fullname)) {
-      formMessage.innerHTML = `<div class="alert alert-danger">Full Name must contain only letters and spaces.</div>`;
+      formMessage.innerHTML = `<div class="alert alert-danger text-center">Full Name must contain only letters and spaces.</div>`;
       return;
   }
 
   if (!email) {
-      formMessage.innerHTML = `<div class="alert alert-danger">Email is required.</div>`;
+      formMessage.innerHTML = `<div class="alert alert-danger text-center">Email is required.</div>`;
       return;
   }
 
   if (!emailRegex.test(email)) {
-      formMessage.innerHTML = `<div class="alert alert-danger">Please provide a valid email address.</div>`;
+      formMessage.innerHTML = `<div class="alert alert-danger text-center">Please provide a valid email address.</div>`;
       return;
   }
 
   if (!message) {
-      formMessage.innerHTML = `<div class="alert alert-danger">Message cannot be empty.</div>`;
+      formMessage.innerHTML = `<div class="alert alert-danger text-center">Message cannot be empty.</div>`;
       return;
   }
 
   // Send data to API
   try {
-      formMessage.innerHTML = 'Sending...';
+      formMessage.innerHTML = '<div class="text-center">Sending... </div>';
 
       const response = await fetch('https://myportfolio-api-877i.onrender.com/message', {
           method: 'POST',
@@ -92,14 +92,14 @@ document.getElementById('contactForm').addEventListener('submit', async function
       const result = await response.json();
 
       if (response.ok) {
-          formMessage.innerHTML = `<div class="alert alert-success">${result.message}</div>`;
+          formMessage.innerHTML = `<div class="alert alert-success text-center">Message sent! I will get back to you as soon as possible.</div>`;
           document.getElementById('contactForm').reset();
       } else {
-          formMessage.innerHTML = `<div class="alert alert-danger">${result.message}</div>`;
+          formMessage.innerHTML = `<div class="alert alert-danger text-center">${result.message}</div>`;
       }
 
   } catch (error) {
-      formMessage.innerHTML = `<div class="alert alert-danger">Something went wrong. Please try again later.</div>`;
+      formMessage.innerHTML = `<div class="alert alert-danger text-center">Something went wrong. Please try again later.</div>`;
       console.error('Error:', error);
   }
 });
